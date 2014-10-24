1451f28446d8d4c8aea9795eff8b8f6a5e0a1348
Helge <helge.dzierzon@plantandfood.co.nz>
2014-10-24 13:56:07 +1300

    Added a moa project to obtain raw data files.

---
+++ 01_RawSequence/.moa/tmp/moa.FD9azJ.sh
@@ -0,0 +1,5 @@
+#!/bin/bash -el
+
+set -v
+wget -O C05F2ACXX_5_fastq.gz "https://www.dropbox.com/s/g7edjgk8ddpmrn7/C05F2ACXX_5_fastq.gz?dl=1"; wget -O C05F2ACXX_5_fastq.gz "https://www.dropbox.com/s/g7edjgk8ddpmrn7/C05F2ACXX_5_fastq.gz?dl=0";
+
---
+++ 01_RawSequence/.moa/tmp/moa.tW42qg.sh
@@ -0,0 +1,5 @@
+#!/bin/bash -el
+
+set -v
+wget -O C05F2ACXX_5_fastq.gz "https://www.dropbox.com/s/jfs8h61ew16iq8h/C05F2ACXX_5_fastq.gz?dl=1"; wget -O C05F2ACXX_5_fastq.gz "https://www.dropbox.com/s/vcrtcqunuxkwz90/C08L7ACXX_6_fastq.gz?dl=1";
+
