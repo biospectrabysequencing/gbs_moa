2b7898c4aff4f5103dcb358eeb2465d4cc507390
Marcus Davy <marcus.davy@plantandfood.co.nz>
2014-10-24 17:07:07 +1300

    resolve conflict: buildmoa.sh

--- buildMoa.sh
+++ buildMoa.sh
@@ -2,30 +2,16 @@
 
 ## Traverse to each moa directory and reconstruct moa instance
 
-wd=$(pwd)
 
 moaDirs=$(find . -name .moa -type d | grep [[:digit:]] | xargs -I {} dirname {})
 
 for i in $moaDirs
 do
-  cd $wd/$i
-  echo $(pwd)
+  cd $i
   moa new -f simple
-  cd $pwd ## or cd -
+  cd -
 done
 
 
 exit;
-
-cd $pwd/02_TagCounts/01_IndividualTagCounts/
-moa new -f simple
-
-cd $pwd/02_TagCounts/02_MergedTagCounts
-moa new -f simple
-
-cd $pwd/02_TagCounts/03_TagCountToFastq
-moa new -f simple
-
-cd $pwd/03_SAM
-moa new -f simple
 
