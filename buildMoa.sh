8e16a94e472827f3e791fa798a9c68718de0cd74
Marcus Davy <marcus.davy@plantandfood.co.nz>
2014-10-28 14:13:26 +1300

    moa templates

--- buildMoa.sh
+++ buildMoa.sh
@@ -1,7 +1,8 @@
 #!/bin/env bash
 
 ## Traverse to each moa directory and reconstruct moa instance
-
+## The moa constuctor below `ma new f simple` needs to know the
+## template in advance per directory 
 
 moaDirs=$(find . -name .moa -type d | grep [[:digit:]] | xargs -I {} dirname {})
 
