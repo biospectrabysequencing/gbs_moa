bf99d87df24b9f9ab2d6fc24de839c8fa78d90db
Marcus Davy <marcus.davy@plantandfood.co.nz>
2014-10-28 14:35:30 +1300

    fetching template from .moa/config

--- buildMoa.sh
+++ buildMoa.sh
@@ -1,15 +1,18 @@
 #!/bin/env bash
 
 ## Traverse to each moa directory and reconstruct moa instance
-## The moa constuctor below `ma new f simple` needs to know the
-## template in advance per directory 
+## The moa constuctor also fetches the remplate required from an additional yaml field
+## via  `moa new f [template]` 
+
 
 moaDirs=$(find . -name .moa -type d | grep [[:digit:]] | xargs -I {} dirname {})
 
 for i in $moaDirs
 do
   cd $i
-  moa new -f simple
+  ## fetch moa template under .moa/config
+  template=$(perl -MYAML=LoadFile -MData::Dumper -le '$f = LoadFile ".moa/config"; print $f->{"template"}')
+  moa new -f $template
   cd -
 done
 
