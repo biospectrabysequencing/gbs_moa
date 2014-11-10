4807da97569fc4dc5db3e9ab1cb7f5111ad81900
Marcus Davy <marcus.davy@plantandfood.co.nz>
2014-11-10 23:16:08 +1300

    simplify

--- test/testMoa.sh
+++ test/testMoa.sh
@@ -10,21 +10,9 @@ title: Find location of config file
 template: simple
 EOF
 
-## Perl dependency YAML::XS
-## cpan
-## get YAML::XS
-## make YAML::XS
-## test YAML::XS
-## install YAML::XS
-
-
-## Test version installed -0.52
-## perl -MYAML::XS -e 'print $YAML::XS::VERSION'
-
 cd /tmp/$USER
 
 ## Execute - similar to line 15 of https://github.com/mdavy86/gbs_moa/blob/master/buildMoa.sh
-## template=$(perl -MYAML::XS=LoadFile -le '$f = LoadFile ".moa/config"; print $f->{"template"}')
 moa new -f simple
 moa run
 
